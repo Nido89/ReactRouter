f77efd530d2b02386e97453ee695327b47d3a261
Jalal Naveed <syednaveedjalal@gmail.com>
2018-12-17 09:36:43 +0200

    routing in component

---
+++ src/Routes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { 
+    BrowserRouter as Router,
+     Route,
+      Link 
+    } from 'react-router-dom';
+    import App from './App';
+
+
+    const customRoutes = () =>{
+
+
+        <Router>
+            <div>
+                <Route path='/' component={App} />
+                {/* <Route path='/about' component={about} /> */}
+
+
+
+              
+
+
+  
+
+
+
+
+            </div>
+        </Router>
+
+
+
+
+    }
+
+    export default customRoutes;
